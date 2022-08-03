005fad20738cbe52761d774c73f137f2ec131b51
kessius00 <kes.olling00@gmail.com>
2022-08-03 23:44:45 +0200

    Add function playRound

--- script.js
+++ script.js
@@ -10,5 +10,33 @@ function getComputerChoice(){
         choiceEmbodiment = 'Scissors';
     };
 
-    console.log(choiceEmbodiment);
+    return choiceEmbodiment
     };
+
+
+function playRound(playerSelection, computerSelection){
+    playerSelection = playerSelection.toUpperCase();
+    computerSelection = computerSelection.toUpperCase();
+    let tie = 'Tie! Try again.'
+
+    if (playerSelection == "ROCK" && computerSelection == "ROCK"){
+        return tie
+    } else if (playerSelection == "PAPER" && computerSelection == "PAPER"){
+        return tie
+    } else if (playerSelection == "SCISSORS" && computerSelection == "SCISSORS"){
+        return tie
+    } else if (playerSelection == "ROCK" && computerSelection == "SCISSORS"){
+        return `You win! ${playerSelection} beats ${computerSelection}!`
+    } else if (playerSelection == "ROCK" && computerSelection == "PAPER"){
+        return `You lose! ${playerSelection} loses to ${computerSelection}!`
+    } else if (playerSelection == "PAPER" && computerSelection == "ROCK"){
+        return `You win! ${playerSelection} beats ${computerSelection}!`
+    } else if (playerSelection == "PAPER" && computerSelection == "SCISSORS"){
+        return `You lose! ${playerSelection} loses to ${computerSelection}!`
+    } else if (playerSelection == "SCISSORS" && computerSelection == "PAPER"){
+        return `You win! ${playerSelection} beats ${computerSelection}!`
+    } else if (playerSelection == "SCISSORS" && computerSelection == "ROCK"){
+        return `You lose! ${playerSelection} loses to ${computerSelection}!`
+    }; 
+};
+
