8e1bb394eaf3f5f98864c350ad7c4bbc2b7c2193
kessius00 <kes.olling00@gmail.com>
2022-08-18 01:19:51 +0200

    Javascript eventListener, playround and pFooter

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-const button = document.querySelector('button');
+const button = document.querySelector('.btn');
 const choices = document.querySelectorAll('.card');
 const rock = document.querySelector('.rock');
 const paper = document.querySelector('.paper');
 const scissors = document.querySelector('.scissors');
 const header = document.querySelector('.header');
+const footer = document.querySelector('.footer');
 const score = document.querySelector('.score');
+let computerScore = 0;
+let humanScore = 0;
 
 button.addEventListener('click', () =>{
     const pScore = document.createElement('p');
@@ -13,8 +16,19 @@ button.addEventListener('click', () =>{
     header.removeChild(button);
     header.insertBefore(pScore, score);
     playAudio(6);
-    
 });
+
+choices.forEach((card) => {
+    card.addEventListener('click', ()=>{
+        let playerSelection = card.classList[1];
+        const pFooter = document.createElement('p');
+        let round = playRound(playerSelection);
+        pFooter.textContent = round;
+        footer.appendChild(pFooter);
+        
+    });
+});
+
 
 
 function playAudio(e){
@@ -23,7 +37,13 @@ function playAudio(e){
     audio.play();
 };
 
-
+// function getPlayerChoice(){
+//     choices.forEach((card) => {
+//         card.addEventListener('click', ()=>{
+//             return card.classList[1]
+//         });
+//     });
+// };
 
 function getComputerChoice(){
     let choiceEmbodiment = '';
@@ -39,9 +59,9 @@ function getComputerChoice(){
     };
 
 
-function playRound(playerSelection, computerSelection){
+function playRound(playerSelection){
     playerSelection = playerSelection.toUpperCase();
-    computerSelection = computerSelection.toUpperCase();
+    computerSelection = getComputerChoice().toUpperCase();
     let tie = 'Tie! Try again.'
 
     if (playerSelection == "ROCK" && computerSelection == "ROCK"){
@@ -65,3 +85,27 @@ function playRound(playerSelection, computerSelection){
     }; 
 };
 
+function determineWinner()
+
+
+
+
+    // // PRINTING THE RESULTS
+    // console.log(getResultRound);
+    
+    // CHECKING SENTENCE FOR CHAR WHICH DECIDES IF WIN
+    // if (getResultRound.charAt(4) == 'w'){
+    //     playerPoints++;
+    // } else if (getResultRound.charAt(4) == 'l'){
+    //     computerPoints++;};
+
+    // // SHOWING THE SCORE 
+    // console.log(`Points of player: ${playerPoints}`);
+    // console.log(`Points of computer: ${computerPoints}`);
+
+    // // DECLARATION OF THE WINNER OF THE GAME
+    // if (playerPoints < computerPoints){
+    //     console.log('Computer wins!');
+    // } else if (playerPoints > computerPoints){
+    //     console.log('Player wins!');
+    // } else {console.log("It's a tie!");};
